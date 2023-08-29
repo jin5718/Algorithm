515a8097e08c8b8f7c390a2103bdeb855cd4f1e3
jin5718 <jin5718@naver.com>
2023-08-29 19:23:16 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,16 +1,11 @@
-arr = []
 lst = []
-lst2 = []
-for i in range(28):
-    num = int(input())
-    arr.append(num)
 
 for j in range(1, 31):
     lst.append(j)
 
+for i in range(28):
+    num = int(input())
+    lst.remove(num)
 
-for k in lst:
-    if k not in arr:
-        lst2.append(k)
-print(min(lst2))
-print(max(lst2))
+print(min(lst))
+print(max(lst))
