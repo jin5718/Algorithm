b59a17ae7be3d1a6638b98b921283a074db84c95
jin5718 <jin5718@naver.com>
2023-11-28 21:37:57 +0900

    [Silver V] Title: 색종이, Time: 144 ms, Memory: 110668 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,16 @@
+N = int(input())
+arr = [[0] * 101 for _ in range(101)]
+for _ in range(N):
+    x, y = map(int, input().split())
+    cnt = 0
+
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            arr[i][j] = 1
+
+    for i in range(101):
+        for j in range(101):
+            if arr[i][j] == 1:
+                cnt += 1
+
+print(cnt)
