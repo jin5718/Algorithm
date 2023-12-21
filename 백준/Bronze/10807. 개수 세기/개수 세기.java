900a2537aaa5ba5063d7706d60366190450ab918
jin5718 <jin5718@naver.com>
2023-12-21 16:32:51 +0900

    [Bronze V] Title: 개수 세기, Time: 216 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		int count = 0;
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int b = sc.nextInt();
+
+		for (int j = 0; j < arr.length; j++) {
+			if (b == arr[j]) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
